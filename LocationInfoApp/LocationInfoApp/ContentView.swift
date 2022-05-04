d8715538879005e45313d680d68d7d4e9704f876
(Mac) Avraam <Georgiadis.ab@gmail.com>
2022-05-04 21:07:09 +0200

    feat:Imported the assets

---
+++ LocationInfoApp/LocationInfoApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LocationInfoApp
+//
+//  Created by Avraam Georgiadis on 2022-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("highlands").resizable()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationInfoApp/LocationInfoApp/LocationInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationInfoAppApp.swift
+//  LocationInfoApp
+//
+//  Created by Avraam Georgiadis on 2022-05-04.
+//
+
+import SwiftUI
+
+@main
+struct LocationInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
